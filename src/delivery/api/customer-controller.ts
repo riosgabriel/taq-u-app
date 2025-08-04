6b19eadf5bb535946f161e4ba054b40b12f48101
Gabriel Rios <grios@justos.com>
2025-08-04 18:12:50 -0300

    refactor: move apis to controllers

---
+++ src/delivery/api/customer-controller.ts
@@ -0,0 +1,64 @@
+import Customer from "@delivery/domain/customer"
+import { CustomerCreateInput, CustomerResponse } from "@delivery/dto/customer-dto"
+import { CustomerRepositoryLive } from "@delivery/repository/customer-repository"
+import { CustomerService, CustomerServiceLive } from "@delivery/services/customer-service"
+import { PrismaLive } from "@delivery/services/prisma-service"
+import { Effect } from "effect"
+import { Request, Response, Router } from "express"
+
+export const CustomerController = Router()
+
+CustomerController.get("/", async (_req, res: Response<Customer[] | { message: string }>) => {
+  const customersEffect = CustomerService.pipe(
+    Effect.andThen((customerService) => customerService.getCustomers()),
+    Effect.map((customers: Customer[]) => {
+      const customersResponse = customers.map(CustomerResponse.fromCustomer)
+      return res.json(customersResponse)
+    }),
+    Effect.catchAll((error) =>
+      Effect.sync(() => {
+        console.error(error)
+        res.status(500).json({ message: "Internal Server Error" })
+      })
+    )
+  ).pipe(Effect.provide(CustomerServiceLive), Effect.provide(CustomerRepositoryLive), Effect.provide(PrismaLive))
+
+  Effect.runPromise(customersEffect)
+})
+
+CustomerController.get("/:id", async (req: Request, res: Response<CustomerResponse | { message: string }>) => {
+  const customerId = req.params.id
+
+  const customerByIdEffect = CustomerService.pipe(
+    Effect.andThen((customerService) => customerService.getCustomerById(customerId)),
+    Effect.map((customer: Customer) => {
+      const customerResponse = CustomerResponse.fromCustomer(customer)
+      return res.json(customerResponse)
+    }),
+    Effect.catchTag("delivery/CustomerNotFoundError", (error) =>
+      Effect.sync(() => res.status(404).json({ message: error.message }))
+    )
+  ).pipe(Effect.provide(CustomerServiceLive), Effect.provide(CustomerRepositoryLive), Effect.provide(PrismaLive))
+
+  Effect.runPromise(customerByIdEffect)
+})
+
+CustomerController.post("/", async (req: Request, res: Response<CustomerResponse | { message: string }>) => {
+  const customerInput = CustomerCreateInput.make(req.body)
+
+  const customerCreatedEffect = CustomerService.pipe(
+    Effect.andThen((customerService) => customerService.createCustomer(customerInput)),
+    Effect.map((customer: Customer) => {
+      const response = CustomerResponse.fromCustomer(customer)
+      return res.json(response)
+    }),
+    Effect.catchAll((error) =>
+      Effect.sync(() => {
+        console.error(error)
+        res.status(500).json({ message: "Internal Server Error" })
+      })
+    )
+  ).pipe(Effect.provide(CustomerServiceLive), Effect.provide(CustomerRepositoryLive), Effect.provide(PrismaLive))
+
+  Effect.runPromise(customerCreatedEffect)
+})
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,7 @@
-import express, { Request, Response } from "express"
+import { CustomerController } from "@delivery/api/customer-controller"
 import dotenv from "dotenv"
-import { CustomerRepositoryLive } from "@delivery/repository/customer-repository"
-import { Effect, Console } from "effect"
-import { PrismaLive } from "@delivery/services/prisma-service"
-import { CustomerService, CustomerServiceLive } from "@delivery/services/customer-service"
-import Customer from "@delivery/domain/customer"
-import { CustomerCreateInput, CustomerResponse } from "@delivery/dto/customer-dto"
+import { Console, Effect } from "effect"
+import express from "express"
 
 dotenv.config()
 
@@ -13,57 +9,13 @@ const startServer = Effect.suspend(() => {
   const app = express()
   const PORT = 3000 // Use config provider
 
+  const apiRouter = express.Router()
+
+  apiRouter.use("/customers", CustomerController)
+
+  app.use("/api", apiRouter)
+
   app.use(express.json())
-
-  app.get("/customers", async (_req, res: Response<Customer[]>) => {
-    const customersEffect = CustomerService.pipe(
-      Effect.andThen((customerService) => customerService.getCustomers()),
-      Effect.provide(CustomerServiceLive),
-      Effect.provide(CustomerRepositoryLive),
-      Effect.provide(PrismaLive)
-    )
-
-    const customers = await Effect.runPromise(customersEffect)
-
-    res.json(customers)
-  })
-
-  app.get("/customers/:id", async (req: Request, res: Response<CustomerResponse | { message: string }>) => {
-    const customerId = req.params.id
-
-    const customerByIdEffect = CustomerService.pipe(
-      Effect.andThen((customerService) => customerService.getCustomerById(customerId)),
-      Effect.provide(CustomerServiceLive),
-      Effect.provide(CustomerRepositoryLive),
-      Effect.provide(PrismaLive)
-    ).pipe(
-      Effect.map((customer: Customer) => {
-        const customerResponse = CustomerResponse.fromCustomer(customer)
-        return res.json(customerResponse)
-      }),
-      Effect.catchTag("delivery/CustomerNotFoundError", (error) =>
-        Effect.sync(() => res.status(404).json({ message: error.message }))
-      )
-    )
-
-    await Effect.runPromise(customerByIdEffect)
-  })
-
-  app.post("/customers", async (req: Request, res: Response<CustomerResponse>) => {
-    const customerInput = CustomerCreateInput.make(req.body)
-
-    const customerCreatedEffect = CustomerService.pipe(
-      Effect.andThen((customerService) => customerService.createCustomer(customerInput)),
-      Effect.provide(CustomerServiceLive),
-      Effect.provide(CustomerRepositoryLive),
-      Effect.provide(PrismaLive),
-      Effect.map((customer: Customer) => CustomerResponse.fromCustomer(customer))
-    )
-
-    const customerResponse = await Effect.runPromise(customerCreatedEffect)
-
-    res.json(customerResponse)
-  })
 
   return Effect.try(() => app.listen(PORT)).pipe(
     Effect.tap((_) => Console.log(`Server is running on http://localhost:${PORT}`))
