9a16c1ac2ab2b0f6049ae9949bfcca1788076bf0
Gabriel Rios <grios@justos.com>
2025-08-10 13:04:28 -0300

    fix: json parser middleware

--- src/index.ts
+++ src/index.ts
@@ -10,14 +10,14 @@ const startServer = Effect.suspend(() => {
   const app = express()
   const PORT = 3000 // Use config provider
 
+  app.use(express.json())
+
   const apiRouter = express.Router()
 
   apiRouter.use("/customers", CustomerController)
   apiRouter.use("/orders", OrderController)
 
   app.use("/api", apiRouter)
-
-  app.use(express.json())
 
   return Effect.try(() => app.listen(PORT)).pipe(
     Effect.tap((_) => Console.log(`Server is running on http://localhost:${PORT}`))
