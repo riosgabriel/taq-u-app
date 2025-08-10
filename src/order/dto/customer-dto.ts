0018815d0ba42f80f7c7a4de5d2c0253d55c0f8a
Gabriel Rios <grios@justos.com>
2025-08-10 13:05:43 -0300

    feat: improve Customer dto schema

--- src/order/dto/customer-dto.ts
+++ src/order/dto/customer-dto.ts
@@ -2,10 +2,22 @@ import { Schema } from "effect/index"
 import Customer from "order/domain/customer"
 
 export class CustomerCreateInput extends Schema.Class<CustomerCreateInput>("CustomerCreateInput")({
-  name: Schema.NonEmptyString,
-  email: Schema.NonEmptyString,
-  phone: Schema.String,
-  address: Schema.NonEmptyString,
+  name: Schema.NonEmptyString.annotations({
+    required: true,
+    identifier: "name",
+  }),
+  email: Schema.NonEmptyString.annotations({
+    required: true,
+    identifier: "email",
+  }),
+  phone: Schema.String.annotations({
+    required: true,
+    identifier: "phone",
+  }),
+  address: Schema.NonEmptyString.annotations({
+    required: true,
+    identifier: "address",
+  }),
 }) {}
 
 export class CustomerResponse extends Schema.Class<CustomerResponse>("CustomerResponse")({
