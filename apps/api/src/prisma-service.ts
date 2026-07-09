aa82445030b82fc220f7cd6d52c96b7cc3f3f702
Gabriel Rios <gabriel.rios@stay.com.br>
2026-07-09 19:55:41 -0300

    feat: PrismaService resource management

--- apps/api/src/prisma-service.ts
+++ apps/api/src/prisma-service.ts
@@ -1,9 +1,12 @@
-import { Context, Layer } from "effect"
 import { PrismaClient } from "@prisma/client"
+import { Context, Effect, Layer } from "effect"
 
 export class PrismaService extends Context.Tag("PrismaService")<PrismaService, { prisma: PrismaClient }>() {}
 
-export const PrismaLive = Layer.sync(PrismaService, () => {
-  const prisma = new PrismaClient()
-  return { prisma }
-})
+export const PrismaLive = Layer.scoped(
+  PrismaService,
+  Effect.acquireRelease(
+    Effect.sync(() => ({ prisma: new PrismaClient() })),
+    ({ prisma }) => Effect.sync(() => prisma.$disconnect())
+  )
+)
